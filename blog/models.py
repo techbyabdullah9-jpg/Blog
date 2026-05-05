a70d76825bcd855604586e313c0ef37800b86f31
Abdullah <techbyabdullah9@gmail.com>
2026-05-05 15:45:55 +0500

    update

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True , blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     short_description = models.CharField(max_length=150)
     content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
