a7daa1c1beadc62f45bb53021401abeedc06a04d
Abdullah <techbyabdullah9@gmail.com>
2026-05-05 15:46:33 +0500

    Add categories to home view contextupdate

--- core/views.py
+++ core/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
-from blog.models import Post
+from blog.models import Category, Post
 
 
 def home(request):
     posts = Post.objects.all()   
+    categories  = Category.objects.all() 
     context = {
-            'posts': posts
+            'posts': posts ,
+            'categories': categories 
         }
     
     return  render(request, 'index.html', context)
